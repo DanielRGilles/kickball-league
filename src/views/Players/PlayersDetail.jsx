1c7c3274a59e670a18520b34b3b22cb1f2ac1679
DanielRGilles <danryangilles@gmail.com>
2021-12-07 19:20:26 -0800

    rendering player and team detail pages

--- src/views/Players/PlayersDetail.jsx
+++ src/views/Players/PlayersDetail.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import { getPlayerById } from '../../services/players'
 
 export default function PlayersDetail() {
+    const { id } = useParams()
+    const [ player, setPlayer ] = useState(null)
+    const [ loading, setLoading ] = useState(true)
+
+    useEffect(() => {
+        getPlayerById(id)
+        .then((data) => setPlayer(data))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false))
+    }, [id] )
+    if (loading) return <h1>Loading player...</h1>
     return (
         <div>
+            <div>{player.name}</div>
+            <div>{player.position}</div>
             
         </div>
     )
--- src/views/Teams/TeamsDetail.jsx
+++ src/views/Teams/TeamsDetail.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { getTeamById } from '../../services/teams.jsx'
+
 
 export default function TeamsDetail() {
+    const { id } = useParams()
+    const [ team , setTeam ] = useState()
+    const [ loading , setLoading ] = useState(true)
+    
+    useEffect(() => {
+        getTeamById(id)
+        .then((data) => setTeam(data))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false))
+    }, [id] )
+
+    if (loading) return <h1>Loading team...</h1>
     return (
-        <div>
+            <ul> {console.log(team)}
+               <li>{team.name}</li>
+               <li>{team.city}</li>
+               <li>{team.state}</li>
+               {team.players.map((player) => <li key={player.id}>{player.name}</li>)}
+           
+            </ul>
             
-        </div>
     )
 }
